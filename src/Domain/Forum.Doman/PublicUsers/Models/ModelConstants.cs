5f1a7a9fbea04fb92de08938905f44f8950c2047
Stenli <stanil_dimitrov@mail.bg>
2020-09-22 15:54:43 +0300

    Add new constant for email regex validation.

--- src/Domain/Forum.Doman/PublicUsers/Models/ModelConstants.cs
+++ src/Domain/Forum.Doman/PublicUsers/Models/ModelConstants.cs
@@ -8,6 +8,7 @@
             public const int MaxNameLength = 20;
             public const int MinEmailLength = 3;
             public const int MaxEmailLength = 50;
+            public const string EmailRegularExpression = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
             public const int MaxUrlLength = 2048;
             public const int Zero = 0;
         }
