4f101c8e26caece435cfe872c570192b531ede90
Stenli <stanil_dimitrov@mail.bg>
2020-09-22 12:45:18 +0300

    Add specifications for user in domain layer.

---
+++ src/Domain/Forum.Doman/PublicUsers/Specifications/Users/UserByIdSpecification.cs
@@ -0,0 +1,20 @@
+﻿using Forum.Doman.Common;
+using Forum.Doman.PublicUsers.Models.Users;
+using System;
+using System.Linq.Expressions;
+
+namespace Forum.Domain.PublicUsers.Specifications.Users
+{
+    public class UserByIdSpecification : Specification<User>
+    {
+        private readonly int? id;
+
+        public UserByIdSpecification(int? id)
+            => this.id = id;
+
+        protected override bool Include => this.id != null;
+
+        public override Expression<Func<User, bool>> ToExpression()
+            => post => post.Id == this.id;
+    }
+}
---
+++ src/Domain/Forum.Doman/PublicUsers/Specifications/Users/UserByUserNameSpecification.cs
@@ -0,0 +1,20 @@
+﻿using Forum.Doman.Common;
+using Forum.Doman.PublicUsers.Models.Users;
+using System;
+using System.Linq.Expressions;
+
+namespace Forum.Domain.PublicUsers.Specifications.Users
+{
+    public class UserByUserNameSpecification : Specification<User>
+    {
+        private readonly string? userName;
+
+        public UserByUserNameSpecification(string? userName) 
+            => this.userName = userName;
+
+        protected override bool Include => this.userName != null;
+
+        public override Expression<Func<User, bool>> ToExpression()
+            => user => user.UserName.ToLower().Contains(this.userName!.ToLower());
+    }
+}
